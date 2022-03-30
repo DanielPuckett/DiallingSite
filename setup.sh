61e224ba5ad887a03c7f3c890b3cd5474ade07e6
DanielPuckett <dpuckett@distributel.ca>
2022-03-30 10:41:03 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,5 @@ ln -s /tmp ./tmp
 cp ApacheDiallerSite.conf /etc/apache2/sites-available/
 rm /etc/apache2/sites-enabled/000-default.conf
 ln -s /etc/apache2/sites-available/ApacheDiallerSite.conf /etc/apache2/sites-enabled/ApacheDiallerSite.conf
+a2enmod cgid
+# reload apache2
