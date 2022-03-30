f05b6873c35d8a693c22552b288ba8e818cbe28f
DanielPuckett <dpuckett@distributel.ca>
2022-03-30 10:33:58 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ chown www-data *
 chmod u+r *
 chmod u+rx *.cgi
 ln -s /tmp ./tmp
+cp ApacheDiallerSite.conf /etc/apache2/sites-available/
+rm /etc/apache2/sites-enabled/000-default.conf
+ln -s /etc/apache2/sites-available/ApacheDiallerSite.conf /etc/apache2/sites-enabled/ApacheDiallerSite.conf
