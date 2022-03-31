13a842011448e3942a55ac05a4c618c0e5e7691d
DanielPuckett <dpuckett@distributel.ca>
2022-03-31 09:56:44 -0400

    Update setup.sh
    Give www-data user a .ssh dir for knownhosts file

--- setup.sh
+++ setup.sh
@@ -7,5 +7,8 @@ ln -s /tmp ./tmp
 cp ApacheDiallerSite.conf /etc/apache2/sites-available/
 rm /etc/apache2/sites-enabled/000-default.conf
 ln -s /etc/apache2/sites-available/ApacheDiallerSite.conf /etc/apache2/sites-enabled/ApacheDiallerSite.conf
+cd /var/www
+mkdir .ssh
+chown www-data .ssh
 a2enmod cgid
 # reload apache2
